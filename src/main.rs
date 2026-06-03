cb3093537730fcaa12ba2179c6cb31e3f293f5e3
dealerofallthecats <xhackerqwertyx@protonmail.com>
2026-06-03 12:27:47 +1200

    add nice colours and spinner

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
+use std::io::{self, Write};
+use colored::*;
+use spinoff::{Spinner, spinners, Color};
+
 pub mod consts;
 pub mod ai;
+pub mod utils;
 
 fn main() {
     //println!("{}", consts::generate_full_prompt("What are the animal cousins of the rabbit?", vec!["User: \"what are the animal cousins of the dingo?\"", "AI: \"The New Guinea Singing Dog and East Asian dogs are close cousins to the dingo\""]));
 
-    let response = ai::get_ai_response(&"Who are the cousins to the dingo?".to_string(), &"gemma4:e2b".to_string());
 
-    println!("Response: {}", response);
+    
+    let mut skin = termimad::MadSkin::default();
+    utils::set_colours(&mut skin);
+    let area = termimad::Area::full_screen();
+
+    loop {
+        let mut input = String::new();
+
+        print!(" {}  ", ">".bold().blue());
+
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut input)
+            .unwrap();
+
+        let mut thinking_loading = Spinner::new(spinners::Dots3, "Thinking ...", Color::Blue);
+        
+        let response = ai::get_ai_response(&input, &"gemma4:e2b".to_string());
+
+        let formatted_content = termimad::FmtText::from(&skin, &response, Some(area.width.into()));
+
+        thinking_loading.success("Finished.");
+
+        println!("\n{}", formatted_content);
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,13 @@
+use termimad::crossterm::style;
+
+
+pub fn set_colours(skin: &mut termimad::MadSkin) {
+    skin.set_fg(style::Color::Rgb { r: 210, g: 210, b: 210 }); // orange-ish              
+                                                                             
+    skin.bold.set_fg(style::Color::Rgb { r: 255, g: 255, b: 255 });                     
+                                                                            
+    skin.italic.set_fg(style::Color::Rgb { r: 180, g: 180, b: 180 });                   
+                                                                             
+    skin.code_block.set_fg(style::Color::Rgb { r: 200, g: 200, b: 200 });
+    skin.code_block.set_bg(style::Color::Rgb { r: 30, g: 30, b: 30 }); 
+}
