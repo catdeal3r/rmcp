0b9aa6cd7a84905ebe1c429fdc971537f7e9b809
dealerofallthecats <xhackerqwertyx@protonmail.com>
2026-06-03 12:42:02 +1200

    need to fix errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::io::{self, Write};
-use colored::*;
 use spinoff::{Spinner, spinners, Color};
 
 pub mod consts;
@@ -9,30 +7,15 @@ pub mod utils;
 fn main() {
     //println!("{}", consts::generate_full_prompt("What are the animal cousins of the rabbit?", vec!["User: \"what are the animal cousins of the dingo?\"", "AI: \"The New Guinea Singing Dog and East Asian dogs are close cousins to the dingo\""]));
 
-
-    
-    let mut skin = termimad::MadSkin::default();
-    utils::set_colours(&mut skin);
-    let area = termimad::Area::full_screen();
-
     loop {
-        let mut input = String::new();
+        let input = utils::get_input();
 
-        print!(" {}  ", ">".bold().blue());
-
-        io::stdout().flush().unwrap();
-
-        io::stdin()
-            .read_line(&mut input)
-            .unwrap();
-
-        let mut thinking_loading = Spinner::new(spinners::Dots3, "Thinking ...", Color::Blue);
+        let mut thinking_loading = Spinner::new(spinners::Line, "Thinking ...", Color::Blue);
         
         let response = ai::get_ai_response(&input, &"gemma4:e2b".to_string());
-
-        let formatted_content = termimad::FmtText::from(&skin, &response, Some(area.width.into()));
-
         thinking_loading.success("Finished.");
+
+        let formatted_content = utils::format_markdown_content(&response);
 
         println!("\n{}", formatted_content);
     }
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
-use termimad::crossterm::style;
-
+use termimad::{crossterm::style, FmtText};
+use std::io::{self, Write};
+use colored::*;
 
 pub fn set_colours(skin: &mut termimad::MadSkin) {
     skin.set_fg(style::Color::Rgb { r: 210, g: 210, b: 210 }); // orange-ish              
@@ -10,4 +11,27 @@ pub fn set_colours(skin: &mut termimad::MadSkin) {
                                                                              
     skin.code_block.set_fg(style::Color::Rgb { r: 200, g: 200, b: 200 });
     skin.code_block.set_bg(style::Color::Rgb { r: 30, g: 30, b: 30 }); 
+}
+
+
+pub fn get_input() -> String {
+    let mut input = String::new();
+
+    print!(" {}  ", ">".bold().blue());
+
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut input)
+        .unwrap();
+
+    input
+}
+
+pub fn format_markdown_content(content: &str) -> FmtText<'_, '_> {
+    let mut skin = termimad::MadSkin::default();
+    self::set_colours(&mut skin);
+    let area = termimad::Area::full_screen();
+    
+    termimad::FmtText::from(&skin, content, Some(area.width.into()))
 }
